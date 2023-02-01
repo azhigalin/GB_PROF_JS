9ce0961b65c11962e3e326f7fdfcad2cf6930799
Aleksandr <101390525+azhigalin@users.noreply.github.com>
2023-02-01 21:59:37 +0300

    Add files 7DZ upload

---
+++ 7DZ_server/public/js/app.js
@@ -0,0 +1,53 @@
+const API =
+  "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses";
+
+const app = new Vue({
+  el: "#app",
+  methods: {
+    getJson(url) {
+      return fetch(url)
+        .then((response) => response.json())
+        .catch((error) => {
+          this.$refs.error.setError(error);
+        });
+    },
+    postJson(url, data) {
+      return fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+        .then((response) => response.json())
+        .catch((error) => {
+          this.$refs.error.setError(error);
+        });
+    },
+    putJson(url, data) {
+      return fetch(url, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+        .then((response) => response.json())
+        .catch((error) => {
+          this.$refs.error.setError(error);
+        });
+    },
+    deleteJson(url) {
+      return fetch(url, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .catch((error) => {
+          this.$refs.error.setError(error);
+        });
+    },
+  },
+});
---
+++ 7DZ_server/public/js/productComp.js
@@ -0,0 +1,49 @@
+Vue.component("products-list", {
+  data() {
+    return {
+      catalogUrl: "/catalogData.json",
+      products: [],
+      filtered: [],
+      imgCatalog: "https://via.placeholder.com/200x150",
+    };
+  },
+  methods: {
+    filter(item) {
+      let regexp = new RegExp(item, "i");
+      this.filtered = this.products.filter((el) =>
+        regexp.test(el.product_name)
+      );
+    },
+  },
+  mounted() {
+    this.$parent.getJson(`/api/products`).then((data) => {
+      for (let element of data) {
+        this.products.push(element);
+        this.filtered.push(element);
+      }
+    });
+  },
+  template: `
+	<div class="products-list">
+	<product v-for="item of filtered" :key="item.id_product" :img="imgCatalog"
+	:product="item">
+	</product>
+	</div>
+	
+	`,
+});
+
+Vue.component("product", {
+  props: ["product", "img"],
+  template: `
+	<div
+	class="product-item"
+>
+	<img :src="img" alt="Some img" />
+	<h3>{{product.product_name}}</h3>
+	<p>{{product.price}}</p>
+	<button class="btn-add" @click="$parent.$parent.$refs.cart.addProduct(product)">
+		Купить
+	</button>
+</div>`,
+});
