78a87d5e2fa1ee3b8e5e38976954b741670e7a06
Aleksandr <101390525+azhigalin@users.noreply.github.com>
2023-02-01 21:56:02 +0300

    Add files 6DZ upload

---
+++ 6DZ_vue_comp/js/app.js
@@ -0,0 +1,18 @@
+const API =
+  "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses";
+
+const app = new Vue({
+  el: "#app",
+  data: {
+    userSearch: "",
+  },
+  methods: {
+    getJson(url) {
+      return fetch(url)
+        .then((response) => response.json())
+        .catch((error) => {
+          this.$refs.error.setError(error);
+        });
+    },
+  },
+});
